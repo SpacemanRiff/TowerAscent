dbce17c0be63743bf45624821e9adbb4230a3d1f
ntp488 <ntp488@gmail.com>
2016-11-17 19:51:51 -0600

    Removes debugs

--- TowerAscent/Assets/Scripts/PivitBallScript.cs
+++ TowerAscent/Assets/Scripts/PivitBallScript.cs
@@ -8,16 +8,10 @@ public class PivitBallScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		print("First: " + transform.position.y);
-		print("Second: " + keepUpTransform.position.y);
-		print("Third: " + transform.localPosition.y);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		print("First: " + transform.position.x);
-		print("Second: " + keepUpTransform.position.y);
-
 		float heightNeeded = keepUpTransform.position.y - transform.position.y;
 
 		if(transform.position.y < maxHeight || heightNeeded < 0) {
