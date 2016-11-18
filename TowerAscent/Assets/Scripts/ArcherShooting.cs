cfe5003e84cc2dd10d151ee85cb78f69347eedf4
Kane Stapler <kanestapler@gmail.com>
2016-11-18 13:40:17 -0600

    Adding reticle and button workings

---
+++ TowerAscent/Assets/Scripts/ArcherShooting.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcherShooting : MonoBehaviour {
+
+	public Texture2D crosshairImage;
+	public const float crosshairMaxSize = 100.0f;
+	public const float crosshairMinSize = 25.0f;
+	public float scalingSpeed = 0.01f;
+	public float scaleMinimum = 0.7f;
+	public GameObject arrow;
+
+	private float currentScale;
+
+	// Use this for initialization
+	void Start () {
+		currentScale = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)) {
+			if(currentScale > scaleMinimum) {
+				currentScale = currentScale - scalingSpeed;
+			}
+		}
+		if(Input.GetMouseButtonUp(0)) {
+			currentScale = 1;
+		}
+	}
+
+	void OnGUI() {
+		float xMin = (Input.mousePosition.x) - ((crosshairMaxSize * currentScale) / 2);
+		float yMin = (Screen.height - Input.mousePosition.y) - ((crosshairMaxSize * currentScale) / 2);
+		GUI.DrawTexture(new Rect(xMin, yMin, crosshairMaxSize * currentScale, crosshairMaxSize * currentScale), crosshairImage);
+	}
+}
--- TowerAscent/Assets/Scripts/ArrowScript.cs
+++ TowerAscent/Assets/Scripts/ArrowScript.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 
 public class ArrowScript : MonoBehaviour {
-
+	
+	public Texture2D crosshairImage;
 	private string controllerTag = "VRController";
+	private string towerTag = "Tower";
+	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +23,16 @@ public class ArrowScript : MonoBehaviour {
 			//if player is grabbing me then do this
 			StartDegrading();
 		}
+		if (other.CompareTag(towerTag)) {
+			//Stop Arrow Movement Entirely
+			rb.velocity = Vector3.zero;
+		}
+	}
+
+	void OnGUI() {
+		float xMin = (Screen.width - Input.mousePosition.x) - (crosshairImage.width / 2);
+		float yMin = (Screen.height - Input.mousePosition.y) - (crosshairImage.height / 2);
+		GUI.DrawTexture(new Rect(xMin, yMin, crosshairImage.width, crosshairImage.height), crosshairImage);
 	}
 
 	private void StartDegrading() {
