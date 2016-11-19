a901e968ebfa8b5c81ef7a3b7d8b8ec2c029b8c0
ntp488 <ntp488@gmail.com>
2016-11-19 00:11:02 -0600

    Makes terrain = awesomesauce

---
+++ TowerAscent/Assets/Scripts/TerrainSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainSettings : MonoBehaviour {
+    Terrain terrain;
+	// Use this for initialization
+	void Start () {
+        terrain = Terrain.activeTerrain;
+        //terrain.terrainData.SetDetailResolution(496, 8);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
