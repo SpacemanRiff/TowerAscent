8af76900d9f078b203d66747b51b8716eedbf0d4
Kane Stapler <kanestapler@gmail.com>
2016-12-01 00:01:16 -0600

    Making bow stuff better. Yay

--- TowerAscent/Assets/Scripts/ArcherShooting.cs
+++ TowerAscent/Assets/Scripts/ArcherShooting.cs
@@ -11,11 +11,12 @@ public class ArcherShooting : MonoBehaviour {
 	public float maxForce = 2.0f;
 	public float scaleMinimum = 0.7f;
 	public float arrowSpeed = 10.0f;
-	public int maxNumberOfArrows = 4, maxNumberOfTracers = 4;
 	public int quiverSize = 3;
+	public float timeBetweenArrows = 1000.0f;
+	private float timeLeft;
 	private int numberArrowsShot = 0;
     public GameObject arrow, arrowTwo, arrowThree, bow;
-    private GameObject arrowShot, bowHold;
+	private GameObject arrowCurrent, bowHold;
     private Camera helperCamera;
 
 	private bool arrowInBow = false;
@@ -23,60 +24,94 @@ public class ArcherShooting : MonoBehaviour {
 	private float currentForce;
 	private float zOffset = 20.0f;
 	private Queue arrowsInGame = new Queue();
-    Vector3 placeToLook, currentMousePosition;
+	private Transform stringOnBow;
+    Vector3 mousePositionWorldSpace, currentMousePosition;
+
+	private 
 
     // Use this for initialization
     void Start () {
 		currentScale = 1;
 		currentForce = 0;
-        helperCamera = GameObject.FindGameObjectWithTag("HelperCamera").GetComponent<Camera>();
+		timeLeft = 0.0f;
+		helperCamera = GameObject.FindGameObjectWithTag("HelperCamera").GetComponent<Camera>();
         bowHold = (GameObject)Instantiate(
                 bow,
                 helperCamera.transform.position
                 + helperCamera.transform.forward * 2
                 + helperCamera.transform.right,
                 transform.rotation);
+		bowHold.transform.position = helperCamera.transform.position
+			+ helperCamera.transform.forward * 2
+			+ helperCamera.transform.right;
+		bowHold.transform.SetParent(helperCamera.transform);
+		stringOnBow = GameObject.FindGameObjectWithTag("String").GetComponent<Transform>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-		placeToLook = helperCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, zOffset));
-
-        bowHold.transform.position = helperCamera.transform.position
-            + helperCamera.transform.forward * 2
-            + helperCamera.transform.right;
-		
-        bowHold.transform.LookAt(placeToLook);
-
-		if(arrowInBow) {
-
-			arrowShot.transform.LookAt(placeToLook);
-
-		} else if(numberArrowsShot < quiverSize) {
-			arrowShot = (GameObject)Instantiate(
-				arrow,
-				helperCamera.transform.position 
-				+ helperCamera.transform.forward * 2 
-				+ helperCamera.transform.right,
-				transform.rotation);
-			arrowsInGame.Enqueue(arrowShot);
-			arrowInBow = true;
-			numberArrowsShot++;
+		mousePositionWorldSpace = helperCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, zOffset));
+        bowHold.transform.LookAt(mousePositionWorldSpace);
+		if(timeLeft >= 0) {
+			timeLeft -= Time.deltaTime;
 		}
 
-		if(Input.GetMouseButton(0)) {
-			if(currentScale > scaleMinimum) {
-				currentScale = currentScale - scalingSpeed;
-				currentForce = currentForce + (1 - currentScale) * maxForce;
+		if(!IsQuiverEmpty()) {
+
+			if(arrowInBow) {
+
+				if(Input.GetMouseButton(0)) {
+					DrawBow();
+				}
+				if(Input.GetMouseButtonUp(0)) {
+					ShootArrow();
+					timeLeft = timeBetweenArrows;
+				}
+
+			} else {
+				if(timeLeft <= 0) {
+					NockArrow();
+				}
 			}
-        }
-		if(Input.GetMouseButtonUp(0)) {
-            arrowShot.GetComponent<Rigidbody>().velocity = arrowShot.transform.forward * arrowSpeed * currentForce;
-			arrowShot.GetComponent<ArrowScript>().shoot();
-            currentScale = 1;
-			currentForce = 1;
-			arrowInBow = false;
+
+
+
 		}
+	}
+
+	private void DrawBow() {
+		if(currentScale > scaleMinimum) {
+			currentScale = currentScale - scalingSpeed;
+			currentForce = currentForce + (1 - currentScale) * maxForce;
+		}
+	}
+
+	private bool IsQuiverEmpty() {
+		return numberArrowsShot >= quiverSize;
+	}
+
+	private void NockArrow() {
+		arrowCurrent = (GameObject)Instantiate(
+			arrow,
+			helperCamera.transform.position
+			+ helperCamera.transform.forward * 2
+			+ helperCamera.transform.right,
+			bowHold.transform.rotation);
+		arrowCurrent.transform.SetParent(stringOnBow);
+		arrowCurrent.GetComponent<Rigidbody>().useGravity = false;
+		arrowsInGame.Enqueue(arrowCurrent);
+		arrowInBow = true;
+	}
+
+	private void ShootArrow() {
+		arrowCurrent.GetComponent<Rigidbody>().useGravity = true;
+		arrowCurrent.transform.SetParent(null);
+		arrowCurrent.GetComponent<Rigidbody>().velocity = arrowCurrent.transform.forward * arrowSpeed * currentForce;
+		arrowCurrent.GetComponent<ArrowScript>().shoot();
+		currentScale = 1;
+		currentForce = 1;
+		arrowInBow = false;
+		numberArrowsShot++;
 	}
 
 	void OnGUI() {
