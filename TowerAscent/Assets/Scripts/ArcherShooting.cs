95c21bacc4de6632f714780d1d35d875a7bba9c2
Todd42 <jkiker@jsu.edu>
2016-12-02 17:29:21 -0600

    Adding in new shooting timing stuff

--- TowerAscent/Assets/Scripts/ArcherShooting.cs
+++ TowerAscent/Assets/Scripts/ArcherShooting.cs
@@ -5,12 +5,6 @@ using System.Collections.Generic;
 public class ArcherShooting : MonoBehaviour {
 
 	public Texture2D crosshairImage;
-	//public Texture2D arrowImage;
-	public const float crosshairMaxSize = 50.0f;
-	public const float crosshairMinSize = 0.1f;
-	public float scalingSpeed = 0.01f;
-	public float maxForce = 2.0f;
-	public float scaleMinimum = 0.7f;
 	public float arrowSpeed = 10.0f;
 	public int quiverSize = 3;
 	public float timeBetweenArrows = 1000.0f;
@@ -20,6 +14,14 @@ public class ArcherShooting : MonoBehaviour {
 	private GameObject arrowCurrent, bowHold;
     private Camera helperCamera;
 	private Animation bowAnimation;
+
+	public float reticleMaxSize = 50.0f;
+	public float reticleMinSize = 0.1f;
+	public float drawBowTime;
+	private float currentDrawTime;
+	private float currentPercentageDrawn;
+	private float currentReticleSize;
+	private float reticleDelta;
 
 	private bool arrowInBow = false;
 	private float currentScale;
@@ -36,6 +38,8 @@ public class ArcherShooting : MonoBehaviour {
 		currentScale = 1;
 		currentForce = 0;
 		timeLeft = 0.0f;
+		reticleDelta = reticleMaxSize - reticleMinSize;
+		ResetDrawMath ();
 		helperCamera = GameObject.FindGameObjectWithTag("HelperCamera").GetComponent<Camera>();
         bowHold = (GameObject)Instantiate(
                 bow,
@@ -84,9 +88,10 @@ public class ArcherShooting : MonoBehaviour {
 
 	private void DrawBow() {
 
-		if(currentScale > scaleMinimum) {
-			currentScale = currentScale - scalingSpeed;
-			currentForce = currentForce + (1 - currentScale) * maxForce;
+		if(currentPercentageDrawn > 0) {
+			currentDrawTime -= Time.deltaTime;
+			currentPercentageDrawn = 1 - (currentDrawTime / drawBowTime);
+			currentReticleSize = reticleMaxSize - (reticleDelta * currentPercentageDrawn);
 			if(!bowAnimation.isPlaying) {
 				bowAnimation["DrawBow"].speed = 1.2f;
 				bowAnimation.Play();
@@ -114,9 +119,11 @@ public class ArcherShooting : MonoBehaviour {
 
 	private void ShootArrow() {
 		arrowCurrent.GetComponent<ArrowScript>().shoot();
-		arrowCurrent.GetComponent<Rigidbody>().velocity = arrowCurrent.transform.forward * arrowSpeed * currentForce;
+		arrowCurrent.GetComponent<Rigidbody>().velocity = arrowCurrent.transform.forward * arrowSpeed * (1-currentPercentageDrawn);
 		bowAnimation.Play();
 		bowAnimation["DrawBow"].speed = -2.0f;
+
+		ResetDrawMath();
 
 		currentScale = 1;
 		currentForce = 1;
@@ -125,11 +132,15 @@ public class ArcherShooting : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		//string arrowsLeftText = "x " + numberArrowsShot;
-		float xMin = (Input.mousePosition.x) - ((crosshairMaxSize * currentScale) / 2);
-		float yMin = (Screen.height - Input.mousePosition.y) - ((crosshairMaxSize * currentScale) / 2);
-		GUI.DrawTexture(new Rect(xMin, yMin, crosshairMaxSize * currentScale, crosshairMaxSize * currentScale), crosshairImage);
-		//GUI.DrawTexture(new Rect(Screen.width - width, Screen.height - height, width, height, arrowImage);
-		//GUI.TextField(new Rect(), arrowsLeftText);
+		float reticleXLocation = Input.mousePosition.x - (reticleMaxSize * currentPercentageDrawn)/2;
+		float reticleYLocation = (Screen.height - Input.mousePosition.y) - ((reticleMaxSize * currentPercentageDrawn) / 2);
+		GUI.DrawTexture (new Rect (reticleXLocation, reticleYLocation, reticleMaxSize * currentPercentageDrawn, reticleMaxSize * currentPercentageDrawn), crosshairImage);
+
+	}
+
+	private void ResetDrawMath() {
+		currentPercentageDrawn = 1;
+		currentDrawTime = drawBowTime;
+		currentReticleSize = reticleMaxSize;
 	}
 }
