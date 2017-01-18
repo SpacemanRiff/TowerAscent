747e3845e445ec0485e8efba87b4b7333a738380
ate134 <jsbolton97@gmail.com>
2017-01-18 12:34:10 -0600

    Level change script & buttons

---
+++ TowerAscent/Assets/LoadLevel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour {
+    public SteamVR_LoadLevel VRLoadLevel;
+    public string levelName;
+
+	// Use this for initialization
+	void Start () {
+        VRLoadLevel.levelName = levelName;
+    }
+
+    void onTriggerEnter(Collider other) {
+        if (other.tag == "VRController") {
+            VRLoadLevel.Trigger();
+        }
+    }
+}
