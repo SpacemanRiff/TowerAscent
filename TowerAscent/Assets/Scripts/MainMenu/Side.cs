ef8b12bac664a9b228b1fd9a23390ffa631a6106
Kane Stapler <kanestapler@gmail.com>
2017-01-30 18:02:16 -0600

    Starting new work on main menu:

---
+++ TowerAscent/Assets/Scripts/MainMenu/Side.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Side {
+
+	private GameObject sideGameObject;
+	private GameObject parent;
+	private Level myLevel;
+
+	public Side(GameObject side) {
+		this.sideGameObject = side;
+	}
+
+	public void SetLevel(Level level) {
+		myLevel = level;
+	}
+
+	public void ScaleSide() {
+		
+	}
+
+	public void GetTransform() {
+	
+	}
+
+
+}
