1035b3c1ff4d37646671435fd47e83668974ef59
Todd42 <jkiker@jsu.edu>
2017-02-03 18:11:59 -0600

    Nextleveler work

---
+++ TowerAscent/Assets/NewNextLeveler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewNextLeveler : MonoBehaviour {
+
+	public SteamVR_LoadLevel SteamVRLevel;
+	public string levelname;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "VRController") {
+			SteamVRLevel.Trigger();
+		}
+	}
+}
